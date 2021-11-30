221e7cc2b9728b1b31d2f233a191306fff7a4560
chris.lee <neoxrock@gmail.com>
2021-11-30 15:59:37 +1100

    loadable preloading

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import React from 'react';
-import { useState, Suspense } from "react";
+import React from "react";
+import { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import loadable from "@loadable/component";
 
-const Split = React.lazy(() => import("./Split"));
+const Split = loadable(() => import("./Split"), {
+  fallback: <div>loading...</div>
+});
 
 function App() {
   const [visible, setVisible] = useState(false);
   const onClick = () => {
     setVisible(true);
   };
+  const onMouseOver = () => {
+    Split.preload();
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p onClick={onClick}>Say Hi</p>
-        <Suspense fallback={<div>loading...</div>}>
-          {visible && <Split />}
-        </Suspense>
+        <p onClick={onClick} onMouseOver={onMouseOver}>Say Hi</p>
+        {visible && <Split />}
       </header>
     </div>
   );
