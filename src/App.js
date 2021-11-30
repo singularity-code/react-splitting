178a93a0d5274cfbcad5cc108683bda2e7963c6a
chris.lee <neoxrock@gmail.com>
2021-11-30 15:43:30 +1100

    dynamic import

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const onClick = () => {
+    import('./notify').then(result => result.default());
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p onClick={onClick}>Say Hi</p>
       </header>
     </div>
   );
